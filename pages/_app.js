b322e2a6e9dcb41876b5acf359b2814bd791a417
Mike Melgren <mikemelgren@gmail.com>
2022-08-11 08:26:34 -0500

    switched to fontsource

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
 import { ContextProvider } from '/utils/Context.jsx'
 import '../styles/globals.css'
+import '@fontsource/raleway/400.css'
+import '@fontsource/raleway/700.css'
 
 //focus outline - accessibility
 const focusShadow = '0 0 0 2px #1dd760'
 
 const theme = extendTheme({
   fonts: {
-    heading: 'Raleway, sans-serif',
-    body: 'Raleway, sans-serif',
+    heading: `'Raleway', sans-serif`,
+    body: `'Raleway', sans-serif`,
   },
   shadows: {
     outline: focusShadow
--- pages/_document.js
+++ pages/_document.js
@@ -32,28 +32,6 @@ import Document, {
           `,
             }}
           />
-            <link
-              rel="preconnect"
-              href="https://fonts.gstatic.com"
-              crossOrigin="true"
-            />
-            <link
-              rel="preload"
-              as="style"
-              href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap"
-            />
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap"
-              media="print"
-              onLoad="this.media='all'"
-            />
-            <noscript>
-              <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap"
-              />
-            </noscript>
           </Head>
           <body>
             <Main />
